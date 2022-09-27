36d64847e91e6a1431ec3cadc736d89b6d7e66dd
bollanarender <97560363+bollanarender@users.noreply.github.com>
2022-09-27 20:52:44 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ChildComponent from "./ChildComponent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     <ChildComponent Narender="Hello child"/>
+    </div>
+  );
+}
---
+++ src/ChildComponent.js
@@ -0,0 +1,7 @@
+export default function ChildComponent({ Narender }) {
+  return (
+    <div>
+      <h1>{Narender}</h1>
+    </div>
+  );
+}
